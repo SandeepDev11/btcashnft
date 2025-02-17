d6b368cf28b44aad3e041d4eacaddb88db6135d1
Sandeep Kumar <sandeep.asvayuktech@gmail.com>
2025-02-17 12:07:52 +0530

    Add Header

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import { Container } from 'react-bootstrap'
+
+const Home = () => {
+  return (
+    <>
+        <main>
+            <Container fluid>
+                <div className='hero-section'>
+
+                </div>
+            </Container>
+        </main>
+    </>
+  )
+}
+
+export default Home
